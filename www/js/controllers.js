ec9b48c87f418c7e1df98b0ef7439e283569418d
Jameson Knoll <j.knoll621@gmail.com>
2016-02-09 15:58:00 +0800

    Integrate Yahoo Finance APIS for financial data

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -4,7 +4,7 @@
 
 var app = angular.module('stockTracker.controllers', []);
 
-app.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
+app.controller('AppCtrl', function($scope, $log, $ionicModal, $timeout) {
 
   // With the new view caching in Ionic, Controllers are only called
   // when they are recreated or on app start, instead of every page change.
@@ -35,7 +35,7 @@ app.controller('AppCtrl', function($scope, $ionicModal, $timeout) {
 
   // Perform the login action when the user submits the login form
   $scope.doLogin = function() {
-    console.log('Doing login', $scope.loginData);
+    $log.info('Doing login', $scope.loginData);
 
     // Simulate a login delay. Remove this and replace with your login
     // code if using a login system
@@ -64,9 +64,31 @@ app.controller('MyStocksCtrl', ['$scope',
   ];
 }]);
 
-app.controller('StockCtrl', ['$scope' , '$stateParams',
-  function($scope, $stateParams) {
+app.controller('StockCtrl', ['$scope' , '$log', '$stateParams', 'StockDataService', function($scope, $log, $stateParams, StockDataService) {
     $scope.ticker = $stateParams.stockTicker;
+
+    $scope.$on('$ionicView.afterEnter', function(){
+      getPriceData();
+      getDetailData();
+    });
+
+
+    function getPriceData() {
+      var promise = StockDataService.getPriceData($scope.ticker);
+
+      promise.then(function(data){
+        $log.info(data);
+      });
+    }
+
+    function getDetailData() {
+      var promise = StockDataService.getDetailData($scope.ticker);
+
+      promise.then(function(data){
+        $log.info(data);
+      });
+    }
+  
 
 }]);
 
--- www/js/services.js
+++ www/js/services.js
@@ -4,4 +4,50 @@
 
 var app = angular.module('stockTracker.services', []);
 
+app.factory('StockDataService', function($log, $q, $http){
+
+	var getDetailData = function(ticker) {
+		
+		var deferred = $q.defer();
+		var url = 'http://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20yahoo.finance.quotes%20where%20symbol%20IN%20(%22' + ticker + '%22)&format=json&env=http://datatables.org/alltables.env';
+
+		$http.get(url)
+    	.success(function(json){
+    		var jsonData = json.query.results.quote;
+    		deferred.resolve(jsonData);
+    	})
+    	.error(function(error){
+    		$log.info('Details data error: ' + error);
+    		deferred.reject();
+    	});
+
+	    return deferred.promise;
+	};
+
+	var getPriceData = function(ticker) {
+
+		var deferred = $q.defer();
+		var url = 'http://finance.yahoo.com/webservice/v1/symbols/' + ticker + '/quote?format=json&view=detail';
+
+		$http.get(url)
+    	.success(function(json){
+    		var jsonData = json.list.resources[0].resource.fields;
+    		deferred.resolve(jsonData);
+    	})
+    	.error(function(error){
+    		$log.info('Price data error: ' + error);
+    		deferred.reject();
+    	});
+
+	    return deferred.promise;
+	    
+	};
+
+	return {
+		getPriceData: getPriceData,
+		getDetailData: getDetailData
+	};
+
+});
+
 })();
